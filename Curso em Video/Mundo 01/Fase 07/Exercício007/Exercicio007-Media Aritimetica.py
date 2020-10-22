30175ba8e0bbd4c2a444f627507c4fd8ee8e5ac8
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-10-22 17:54:24 -0300

    Create Exercicio007-Media Aritimetica.py

---
+++ Curso em Video/Mundo 01/Fase 07/Exercício007/Exercicio007-Media Aritimetica.py
@@ -0,0 +1,3 @@
+n1 = float(input('Digite a primeira nota do aluno: '))
+n2 = float(input('Digite a segunda nota do aluno: '))
+print('A media das notas entre {:.1f} e {:.1f} é igual a {:.1f}.'.format(n1, n2, (n1 + n2) / 2))
