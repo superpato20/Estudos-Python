1a694362ba069c5123e91af444196b19bf584c65
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-10-22 17:54:13 -0300

    Create Exercicio006-Dobro,Triplo,Raiz Quadrada.py

---
+++ Curso em Video/Mundo 01/Fase 07/Exercício006/Exercicio006-Dobro,Triplo,Raiz Quadrada.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um numero: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('O dobro de {} vale {}.'.format(n, d))
+print('O triplo de {} vale {}.'.format(n, t))
+print('A raiz quadrada de {} é igual a {:.2f}.'.format(n, r))
