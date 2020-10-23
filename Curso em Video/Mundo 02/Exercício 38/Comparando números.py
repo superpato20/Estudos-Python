31fad91393da8d6365b3fa30fef50d4ce4b134d2
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-10-23 10:50:52 -0300

    Create Comparando números.py

---
+++ Curso em Video/Mundo 02/Exercício 38/Comparando números.py
@@ -0,0 +1,9 @@
+n1 = int(input('Primeiro numero: '))
+n2 = int(input('Segundo numero: '))
+if n1 > n2:
+    print('O PRIMEIRO valor é maior.')
+elif n1 < n2:
+    print('O SEGUNDO valor é maior.')
+elif n1 == n2:
+    print('Os dois valores são IGUAIS.')
+
