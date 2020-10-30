32119193b7fdc7aa1c66b7442d2193edf6e2be85
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-10-30 15:12:47 -0300

    Create Praticando003-Mundo03 Tupla.py

---
+++ Curso em Video/Mundo 03/Praticando/Praticando003-Mundo03 Tupla.py
@@ -0,0 +1,4 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+print('Comi pra caramba!!!')
