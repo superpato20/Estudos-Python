712b8c4dbf76384eea3f0318fab3c652638cba89
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-10-30 15:12:50 -0300

    Create Praticando004-Mundo03 Tupla.py

---
+++ Curso em Video/Mundo 03/Praticando/Praticando004-Mundo03 Tupla.py
@@ -0,0 +1,4 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+print('Eu comi pra caramba!!!')
