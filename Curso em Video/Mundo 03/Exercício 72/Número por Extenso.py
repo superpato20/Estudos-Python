1b24585e16f4b824b8bec0144758b384b48df0a8
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-10-30 17:51:02 -0300

    Update Número por Extenso.py

--- Curso em Video/Mundo 03/Exercício 72/Número por Extenso.py
+++ Curso em Video/Mundo 03/Exercício 72/Número por Extenso.py
@@ -1,5 +1,6 @@
 num = ' '
-num1 = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis')
+num1 = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
+        'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 while num not in range(0, 21):
     num = int(input('Digite um numero entre 0 e 20: '))
     if num not in range(0, 21):
