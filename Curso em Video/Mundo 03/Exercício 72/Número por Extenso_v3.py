91f8f1ce00d26b459f1af47747def468102a5585
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-10-31 09:48:09 -0300

    Create Número por Extenso_v3.py

---
+++ Curso em Video/Mundo 03/Exercício 72/Número por Extenso_v3.py
@@ -0,0 +1,9 @@
+cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'quatro', 'cinco',
+        'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'teze', 'catorze', 'quinze',
+        'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    num = int(input('Digite um numero entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente Novamente. ', end='')
+print(f'Você digitou o numero {cont[num]}.')
