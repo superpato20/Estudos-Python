507bfed42bfc03ef59a451cf7f4a9dd077dc90d1
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-10-31 11:54:06 -0300

    Update Número por Extenso.py

--- Curso em Video/Mundo 03/Exercício 72/Número por Extenso.py
+++ Curso em Video/Mundo 03/Exercício 72/Número por Extenso.py
@@ -5,4 +5,14 @@ while num not in range(0, 21):
     num = int(input('Digite um numero entre 0 e 20: '))
     if num not in range(0, 21):
         print('Tente Novamente.', end='')
-print(f'Você digitou o numero {num1[num]}')
+    if num in range(0, 21):
+        print(f'Você digitou o numero {num1[num]}')
+        conti = ' '
+        while conti not in 'n':
+            conti = str(input('Quer continuar: [S / N] ')).lower()
+            num = int(input('Digite um numero entre 0 e 20: '))
+            if num not in range(0, 21):
+                print('Tente Novamente.', end='')
+                break
+            if num in range(0, 21):
+                print(f'Você digitou o numero {num1[num]}')
