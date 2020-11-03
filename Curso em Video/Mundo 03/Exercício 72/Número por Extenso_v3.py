35f9d016208f0d24bcd689000f95165a76054744
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-03 09:56:22 -0300

    Update Número por Extenso_v3.py

--- Curso em Video/Mundo 03/Exercício 72/Número por Extenso_v3.py
+++ Curso em Video/Mundo 03/Exercício 72/Número por Extenso_v3.py
@@ -1,19 +1,22 @@
+num = ' '
 cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'quatro', 'cinco',
         'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'teze', 'catorze', 'quinze',
         'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
-while True:
+while num not in range(0, 21):
     num = int(input('Digite um numero entre 0 e 20: '))
-    if 0 <= num <= 20:
-        break
-    print('Tente Novamente. ', end='')
-print(f'Você digitou o numero {cont[num]}.')
-conti = ' '
-while conti not in 'n':
-    conti = str(input('Quer continuar: [S / N] ')).lower()
-    if conti in 'n':
-        break
-    num = int(input('Digite um numero entre 0 e 20: '))
-    if 0 <= num <= 20:
-        break
-    print('Tente Novamente. ', end='')
-print(f'Você digitou o numero {cont[num]}.')
+    if num not in range(0, 21):
+        print('Tente Novamente. ', end='')
+    elif num in range(0, 21):
+        print(f'Você digitou o numero {cont[num]}.')
+        conti = ' '
+        while conti not in 'n':
+            conti = str(input('Quer continuar: [S / N] ')).lower().strip()
+            if conti == 'n':
+                break
+            num = int(input('Digite um numero entre 0 e 20: '))
+            if num not in range(0, 21):
+                print('Tente Novamente. ', end='')
+                break
+            if num in range(0, 21):
+                print(f'Você digitou o numero {cont[num]}.')
+print('Programa encerrado!')
