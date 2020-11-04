c94dd2e92e4c5b0257983216157e86e41d50bfa4
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-04 09:10:11 -0300

    Update Maior e menor valores em Tupla.py

--- Curso em Video/Mundo 03/Exercício 74/Maior e menor valores em Tupla.py
+++ Curso em Video/Mundo 03/Exercício 74/Maior e menor valores em Tupla.py
@@ -1,5 +1,5 @@
 from random import randint
 sorteio = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
-
-print(f'Os valores sorteados foram: {sorteio}')
-
+print(f'Os valores sorteados foram: {sorteio}.'.replace(',', '').replace('(', '').replace(')', ''))
+print(f'O maior valor sorteado foi {max(sorteio)}.')
+print(f'O menor valor sorteado foi {min(sorteio)}.')
