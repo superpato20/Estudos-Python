5c59f2431fb24727c959fdfd4e90db80ee3674d4
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-05 18:01:36 -0300

    Update Análise de dados em uma Tupla.py

--- Curso em Video/Mundo 03/Exercício 75/Análise de dados em uma Tupla.py
+++ Curso em Video/Mundo 03/Exercício 75/Análise de dados em uma Tupla.py
@@ -4,14 +4,16 @@ n3 = int(input('Digite mais um numero: '))
 n4 = int(input('Digite o ultimo numero: '))
 numeros = (n1, n2, n3, n4)
 contador = 0
-if n1 % 2 == 0:
+if numeros in ( n1 ) % 2 == 0:
     contador += 1
-if n2 % 2 == 0:
+'''if n1 % 2 == 0:
+    contador += 1
+elif n2 % 2 == 0:
     contador += 1
 elif n3 % 2 == 0:
     contador += 1
 elif n4 % 2 == 0:
-    contador += 1
+    contador += 1'''
 print(f'Você digitou os valores: {numeros}.'.replace(',', '').replace('(', '').replace(')', ''))
 print(f'O valor 9 apareceu {numeros.count(9)} vezes.')
 print(f'O valor 3 apareceu na {numeros.index(3) + 1}ª posição.')
