5f34c43e3843c003a548d878d0db620e76242870
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-06 13:44:07 -0300

    Create Análise de dados em uma Tupla_2.py

---
+++ Curso em Video/Mundo 03/Exercício 75/Análise de dados em uma Tupla_2.py
@@ -0,0 +1,13 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+n3 = int(input('Digite mais um numero: '))
+n4 = int(input('Digite o ultimo numero: '))
+numeros = (n1, n2, n3, n4)
+contador = 0
+for c in numeros:
+    if c % 2 == 0:
+        contador += 1
+print(f'Você digitou os valores: {numeros}.'.replace(',', '').replace('(', '').replace(')', ''))
+print(f'O valor 9 apareceu {numeros.count(9)} vezes.')
+print(f'O valor 3 apareceu na {numeros.index(3) + 1}ª posição.')
+print(f'Os valores pares digitados foram {contador}')
