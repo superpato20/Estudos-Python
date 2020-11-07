03077114e09cbe3733897536f6942099d6466c5a
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-07 10:29:59 -0300

    Update Lista de Preços com Tupla.py

--- Curso em Video/Mundo 03/Exercício 76/Lista de Preços com Tupla.py
+++ Curso em Video/Mundo 03/Exercício 76/Lista de Preços com Tupla.py
@@ -1,5 +1,12 @@
-listagem = ('''LISTAGEM DE PREÇOS,
-Lápis,
-Borracha
-Caderno''').replace(',', '')
-print(listagem)
+listagem = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90, 'Estojo', 25.00,
+            'Transferidor', 4.20, 'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30,
+            'Livro', 34.90)
+print('=' * 40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-' * 40)
+for c in range(0, len(listagem)):
+    if c % 2 == 0:
+        print(f'{listagem[c]:.<30}', end='')
+    else:
+        print(f'R${listagem[c]:>8.2f}')
+print('-' * 40)
