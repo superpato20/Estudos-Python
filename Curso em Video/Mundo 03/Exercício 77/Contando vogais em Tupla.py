465885a7cb82546e61ecdfc9a1c85676c4f756e9
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-07 11:53:46 -0300

    Update Contando vogais em Tupla.py

--- Curso em Video/Mundo 03/Exercício 77/Contando vogais em Tupla.py
+++ Curso em Video/Mundo 03/Exercício 77/Contando vogais em Tupla.py
@@ -1,11 +1,10 @@
-'''listagem = ('APRENDER', 'PROGRAMAR')
+listagem = ('APRENDER', 'PROGRAMAR')
 for c in listagem:
 
-    print(f'Na palavra {c} temos {c.count("A")}')'''
+    print(f'Na palavra {c} temos {c.count("A")}')
 
-nome = input("Digite o nome: ")
-
+'''nome = input("Digite o nome: ")
 for index, letra in enumerate(nome):
     if letra in "aeiou":
-        print("Encontrado a letra '%c' na posição %d" % (letra, index))
+        print("Encontrado a letra '%c' na posição %d" % (letra, index))'''
 
