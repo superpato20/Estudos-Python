73363151c82d20bf4d47d72123013c5937ba3a90
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-09 10:21:08 -0300

    Update Contando vogais em Tupla.py

--- Curso em Video/Mundo 03/Exercício 77/Contando vogais em Tupla.py
+++ Curso em Video/Mundo 03/Exercício 77/Contando vogais em Tupla.py
@@ -1,4 +1,8 @@
-listagem = ('APRENDER', 'PROGRAMAR')
+listagem = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS',
+            'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
 for c in listagem:
-    print(f'Na palavra {c} temos ')
-
+    print(f'Na palavra {c} temos ', end='')
+    for d in c:
+        if d in 'AEIOU':
+            print(d.lower(), end=' ')
+    print('.')
