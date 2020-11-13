9f4e749575b3f52136b4221c86451718fff602a1
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-13 10:08:56 -0300

    Update Praticando005-Mundo03 Listas.py

--- Curso em Video/Mundo 03/Praticando/Praticando005-Mundo03 Listas.py
+++ Curso em Video/Mundo 03/Praticando/Praticando005-Mundo03 Listas.py
@@ -7,3 +7,5 @@ print(lista)
 del lista[3]               # apaga o elemento requerido da lista
 lista.pop(4)               # apaga o elemento requerido da lista
 print(lista)
+lista.remove('bolo')       # apaga o elemento especifico da lista
+print(lista)
