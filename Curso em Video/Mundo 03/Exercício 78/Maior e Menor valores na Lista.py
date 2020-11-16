5b694f20744853d47c1843fa6ba09303f72f06c6
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-16 10:00:54 -0300

    Update Maior e Menor valores na Lista.py

--- Curso em Video/Mundo 03/Exercício 78/Maior e Menor valores na Lista.py
+++ Curso em Video/Mundo 03/Exercício 78/Maior e Menor valores na Lista.py
@@ -2,5 +2,9 @@ lista = []
 for c in range(0, 5):
     n = int(input(f'Digite um valor para a Posição {c}: '))
     lista.append(n)
-print(lista)
+print(f'Você digitou os valores {lista}.'.replace(',', '').replace('[', '').replace(']', ''))
+print(f'O maior valor digitado foi {max(lista)} nas posições ', end='')
+for c in lista:
+    print(f'{lista.index(max(lista)) + 1}...')
+print(f'O menor valor digitado foi {min(lista)}')
 
