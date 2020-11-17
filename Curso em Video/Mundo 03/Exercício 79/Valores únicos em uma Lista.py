b74872cd35ec67255e9bb850e61c865dcc937e7c
superpato20 <luizduck20@gmail.com>
2020-11-17 20:03:25 -0300

    Update Valores únicos em uma Lista.py

--- Curso em Video/Mundo 03/Exercício 79/Valores únicos em uma Lista.py
+++ Curso em Video/Mundo 03/Exercício 79/Valores únicos em uma Lista.py
@@ -1,9 +1,9 @@
 listanum = []
 while True:
     n = (int(input('Digite um valor: ')))
-    if n in listanum:
+    if n not in listanum:
         print('a')
-    listanum.append(n)
+        listanum.append(n)
     continuar = ' '
     while continuar not in 'sn':
         continuar = str(input('Quer continuar: [S / N] ')).lower().strip()
