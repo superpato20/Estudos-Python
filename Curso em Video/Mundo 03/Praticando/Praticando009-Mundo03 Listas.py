00e94ab693c6b427e551841012d3e0faf4d58f03
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-19 16:10:36 -0300

    Update Praticando009-Mundo03 Listas.py

--- Curso em Video/Mundo 03/Praticando/Praticando009-Mundo03 Listas.py
+++ Curso em Video/Mundo 03/Praticando/Praticando009-Mundo03 Listas.py
@@ -8,3 +8,9 @@ teste[0] = 'Mark'
 teste[1] = '30'
 galera.append(teste[:])
 print(galera)
+galera1 = list()
+galera1.append('Nara')
+
+galera1.append(28)
+galera1.append(teste), galera1.append(galera)
+print(galera1)
