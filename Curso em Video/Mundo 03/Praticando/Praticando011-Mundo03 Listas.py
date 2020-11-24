65a51eb17c2a455902b3c222425256740a761773
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-24 13:27:03 -0300

    Update Praticando011-Mundo03 Listas.py

--- Curso em Video/Mundo 03/Praticando/Praticando011-Mundo03 Listas.py
+++ Curso em Video/Mundo 03/Praticando/Praticando011-Mundo03 Listas.py
@@ -5,4 +5,6 @@ for c in range(0, 3):
     dado.append(int(input('Idade: ')))
     galera.append(dado[:])
     dado.clear()
-print(galera)
+for p in galera:
+    if p[1] >= 21:
+        print(f'{p[0]} é maior de idade.')
