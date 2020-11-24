9f15236922e4d949cb3f06324f18d8bdd8ebc224
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-24 13:30:20 -0300

    Update Praticando011-Mundo03 Listas.py

--- Curso em Video/Mundo 03/Praticando/Praticando011-Mundo03 Listas.py
+++ Curso em Video/Mundo 03/Praticando/Praticando011-Mundo03 Listas.py
@@ -1,5 +1,6 @@
 galera = list()
 dado = list()
+totmaior = totmenor = 0
 for c in range(0, 3):
     dado.append(str(input('Nome: ')))
     dado.append(int(input('Idade: ')))
@@ -7,4 +8,9 @@ for c in range(0, 3):
     dado.clear()
 for p in galera:
     if p[1] >= 21:
+        totmaior += 1
         print(f'{p[0]} é maior de idade.')
+    else:
+        totmenor += 1
+        print(f'{p[0]} é menor de idade.')
+print(f'Temos {totmaior} maiores de idade, e temos {totmenor} menores de idade.')
