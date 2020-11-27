1c78af21c2537c790213947e9bb70970496e1aa9
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-11-27 17:22:59 -0300

    Update Lista composta e análise de dados.py

--- Curso em Video/Mundo 03/Exercício 84/Lista composta e análise de dados.py
+++ Curso em Video/Mundo 03/Exercício 84/Lista composta e análise de dados.py
@@ -1,15 +1,11 @@
 pessoas = maiorpeso = menorpeso = 0
 listamaior = list()
 listamenor = list()
-c = 0
 while True:
     nome = str(input('Nome: '))
     pessoas += 1
     peso = float(input('Peso: '))
-    listamaior.append(nome)
-    listamaior.append(peso)
-    listamenor.append(nome)
-    listamenor.append(peso)
+   
     continuar = ' '
     while continuar not in 'sn':
         continuar = str(input('Quer continuar? [S / N] '))
