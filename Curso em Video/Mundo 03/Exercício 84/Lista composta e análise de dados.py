4943d10d1a9817e22bc9d4bd96ff7ea005ed27d7
superpato20 <luizduck20@gmail.com>
2020-11-30 20:41:37 -0300

    Update Lista composta e análise de dados.py

--- Curso em Video/Mundo 03/Exercício 84/Lista composta e análise de dados.py
+++ Curso em Video/Mundo 03/Exercício 84/Lista composta e análise de dados.py
@@ -1,31 +1,3 @@
-'''pessoas = maiorpeso = menorpeso = 0
-listamaior = list()
-listamenor = list()
-while True:
-    nome = str(input('Nome: '))
-    pessoas += 1
-    peso = float(input('Peso: '))
-
-    if peso >= maiorpeso:
-        listamaior.append(nome)
-        listamaior.append(peso)
-        maiorpeso = peso
-        menorpeso = peso
-    else:
-
-        listamenor.append(nome)
-        listamenor.append(peso)
-        menorpeso = peso
-    continuar = ' '
-    while continuar not in 'sn':
-        continuar = str(input('Quer continuar? [S / N] '))
-    if continuar == 'n':
-        break
-print(f'Ao todo, você cadastrou {pessoas} pessoas.')
-print(listamaior)
-print(listamenor)'''
-
-
 dados = []
 povo = []
 peso = []
@@ -41,12 +13,11 @@ while True:
     if continuar in 'n':
         break
 print(f'Ao todo, você cadastrou {len(povo)} pessoas.')
-print(f'O maior peso foi de {max(peso):.2f}2Kg.Peso de ',end=' ')
+print(f'O maior peso foi de {max(peso):.2f}Kg.Peso de ', end='')
 for p in povo:
     if p[1] == max(peso):
-        print(f'{p[0]}', end=' ')
+        print(f'{p[0]}.', end='')
 print(f'O menor peso foi de {min(peso):.2f}', end=' ')
 for p in povo:
     if p[1] == min(peso):
         print(f'{p[0]}', end=' ')
-
