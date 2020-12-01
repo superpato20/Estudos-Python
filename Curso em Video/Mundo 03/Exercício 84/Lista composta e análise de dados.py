3ff7b8c7fc4d3325708e2d01c8979d63a94dbbad
superpato20 <luizduck20@gmail.com>
2020-11-30 21:07:36 -0300

    Update Lista composta e análise de dados.py

--- Curso em Video/Mundo 03/Exercício 84/Lista composta e análise de dados.py
+++ Curso em Video/Mundo 03/Exercício 84/Lista composta e análise de dados.py
@@ -3,7 +3,7 @@ povo = []
 peso = []
 while True:
     dados.append(str(input('Nome: ')))
-    dados.append(int(input('Peso: ')))
+    dados.append(float(input('Peso: ')))
     povo.append(dados[:])
     peso.append(dados[1])
     dados.clear()
@@ -16,7 +16,8 @@ print(f'Ao todo, você cadastrou {len(povo)} pessoas.')
 print(f'O maior peso foi de {max(peso):.2f}Kg.Peso de ', end='')
 for p in povo:
     if p[1] == max(peso):
-        print(f'{p[0]}.', end='')
+        print(f'{p[0]} ', end='')
+print('.', end='')
 print(f'O menor peso foi de {min(peso):.2f}', end=' ')
 for p in povo:
     if p[1] == min(peso):
