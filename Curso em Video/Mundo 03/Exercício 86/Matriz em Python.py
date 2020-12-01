af28be403a2935e45bb0cfd7d9a1c5682ede3335
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-12-01 17:45:31 -0300

    Create Matriz em Python.py

---
+++ Curso em Video/Mundo 03/Exercício 86/Matriz em Python.py
@@ -0,0 +1,17 @@
+princ = list()
+lista = list()
+lista2 = list()
+lista3 = list()
+for c in range(0, 3):
+    lista.append(int(input('Digite um valor: ')))
+princ.append(lista)
+for d in range(0, 3):
+    lista2.append(int(input('Digite um valor: ')))
+princ.append(lista2)
+for e in range(0, 3):
+    lista3.append(int(input('Digite um valor: ')))
+princ.append(lista3)
+print(lista)
+print(lista2)
+print(lista3)
+print(princ)
