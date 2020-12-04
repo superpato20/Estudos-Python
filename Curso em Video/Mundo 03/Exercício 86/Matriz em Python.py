1290cbbfe821af7fc0586db10ba16a54ef0b90cc
superpato20 <luizduck20@gmail.com>
2020-12-04 00:02:46 -0300

    Update Matriz em Python.py

--- Curso em Video/Mundo 03/Exercício 86/Matriz em Python.py
+++ Curso em Video/Mundo 03/Exercício 86/Matriz em Python.py
@@ -1,17 +1,9 @@
-princ = list()
-lista = list()
-lista2 = list()
-lista3 = list()
+matriz = [[], [], []]
 for c in range(0, 3):
-    lista.append(int(input('Digite um valor: ')))
-princ.append(lista)
-for d in range(0, 3):
-    lista2.append(int(input('Digite um valor: ')))
-princ.append(lista2)
-for e in range(0, 3):
-    lista3.append(int(input('Digite um valor: ')))
-princ.append(lista3)
-print(lista)
-print(lista2)
-print(lista3)
-print(princ)
+    for d in range(0, 3):
+        matriz[c].append(int(input(f'Digite um valor para [{c},{d}]: ')))
+print('-=' * 30)
+for c in range(0, 3):
+    for d in range(0, 3):
+        print(f'[{matriz[c][d]:^5}]', end='')
+    print()
