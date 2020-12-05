4c5f53fa9ee9e7f0b9384ca84c634c0d5b76793d
superpato20 <luizduck20@gmail.com>
2020-12-05 15:15:43 -0300

    Update Mais sobre Matriz em Python.py

--- Curso em Video/Mundo 03/Exercício 87/Mais sobre Matriz em Python.py
+++ Curso em Video/Mundo 03/Exercício 87/Mais sobre Matriz em Python.py
@@ -1,10 +1,24 @@
 matriz = [[], [], []]
+pares = 0
 for c in range(0, 3):
     for d in range(0, 3):
-        matriz[c].append(int(input(f'Digite um valor para [{c},{d}]: ')))
-print('-=' * 30)
+        n = (int(input(f'Digite um valor para [{c},{d}]: ')))
+        matriz[c].append(n)
+        if n % 2 == 0:
+            pares += n
+print('-=' * 25)
 for c in range(0, 3):
     for d in range(0, 3):
         print(f'[{matriz[c][d]:^5}]', end='')
     print()
-
+terceiracoluna = 0
+for coluna in range(len(matriz[0])):
+    soma = 0
+    for linha in range(len(matriz)):
+        if matriz[linha][coluna] >= 0:
+            soma += matriz[linha][coluna]
+    terceiracoluna = soma
+print('-=' * 25)
+print(f'A soma dos valores pares é {pares}.')
+print(f'A soma dos valores da terceira coluna é {terceiracoluna}.')
+print(f'O maior valor da segunda linha é {max(matriz[1])}.')
