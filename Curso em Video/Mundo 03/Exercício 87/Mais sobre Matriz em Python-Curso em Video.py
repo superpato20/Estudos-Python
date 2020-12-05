6cad0878d347d9f897e20ed9a70a4e6d943b57c7
superpato20 <luizduck20@gmail.com>
2020-12-05 20:11:44 -0300

    Create Mais sobre Matriz em Python-Curso em Video.py

---
+++ Curso em Video/Mundo 03/Exercício 87/Mais sobre Matriz em Python-Curso em Video.py
@@ -0,0 +1,23 @@
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+spar = mai = scol = 0
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        matriz[linha][coluna] = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
+print('-=' * 25)
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        print(f'[{matriz[linha][coluna]:^5}]', end='')
+        if matriz[linha][coluna] % 2 == 0:
+            spar += matriz[linha][coluna]
+    print()
+for linha in range(0, 3):
+    scol += matriz[linha][2]
+for coluna in range(0, 3):
+    if coluna == 0:
+        mai = matriz[1][coluna]
+    elif matriz[1][coluna] > mai:
+        mai = matriz[1][coluna]
+print('-=' * 25)
+print(f'A soma dos valores pares é {spar}.')
+print(f'A soma dos valores da terceira coluna é {scol}.')
+print(f'O maior valor da segunda linha é {mai}.')
