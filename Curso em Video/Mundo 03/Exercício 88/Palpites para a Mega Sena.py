9d52b0ea8ea4181e13a02f5ad300208b1a0524bc
superpato20 <luizduck20@gmail.com>
2020-12-05 22:30:34 -0300

    Update Palpites para a Mega Sena.py

--- Curso em Video/Mundo 03/Exercício 88/Palpites para a Mega Sena.py
+++ Curso em Video/Mundo 03/Exercício 88/Palpites para a Mega Sena.py
@@ -5,11 +5,8 @@ njogos = int(input('Quantos jogos você quer que eu sorteie? '))
 from random import randint
 from time import sleep
 jogos = []
-n = 0
-while True:
-    for c in range(0, 6):
-        jogos.append(randint(0, 60))
-
+for c in range(0, 6):
+    jogos.append(randint(0, 60))
 print('-=' * 2, end=' ')
 print(f'Sorteando {njogos} Jogos', end=' ')
 print('-=' * 2)
