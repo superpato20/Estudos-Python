7fa6c5584ed514295264561ca296b1a8a0fa50a4
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-12-09 17:33:00 -0300

    Update Boletim com listas compostas.py

--- Curso em Video/Mundo 03/Exercício 89/Boletim com listas compostas.py
+++ Curso em Video/Mundo 03/Exercício 89/Boletim com listas compostas.py
@@ -1,2 +1,17 @@
-
-
+listanome = list()
+listanome2 = list()
+while True:
+    nome = str(input('Nome: '))
+    nota1 = int(input('Nota 1: '))
+    nota2 = int(input('Nota 2: '))
+    listanome.append(nome)
+    listanome.append(nota1)
+    listanome.append(nota2)
+    listanome2.append(listanome[:])
+    listanome.clear()
+    continuar = ' '
+    if continuar not in 'sn':
+        continuar = str(input('Quer continuar? [S / N] ')).lower().strip()
+    if continuar == 'n':
+        break
+print(listanome2)
