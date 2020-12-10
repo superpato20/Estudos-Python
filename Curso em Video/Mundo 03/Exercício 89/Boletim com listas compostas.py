3a1a60805b3907be3cfb2df67fb42ee7c172d6b6
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-12-10 09:36:15 -0300

    Update Boletim com listas compostas.py

--- Curso em Video/Mundo 03/Exercício 89/Boletim com listas compostas.py
+++ Curso em Video/Mundo 03/Exercício 89/Boletim com listas compostas.py
@@ -1,3 +1,4 @@
+from time import sleep
 listanome = list()
 listanome2 = list()
 while True:
@@ -27,4 +28,8 @@ while True:
     listanome3 = list()
     listanome3.append(listanome2[notas])
     for index, item in enumerate(listanome3):
-        print(f'Notas de {item[0]} são {item[1]} e {item[2]}')
+        print(f'Notas de {item[0]} são [{item[1]}, {item[2]}]')
+    print('-' * 30)
+print('FINALIZANDO ...')
+sleep(1.2)
+print('VOLTE SEMPRE!!!')
