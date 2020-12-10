1bb0bd2bd5b347d910394cc8df363051181b2912
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-12-10 17:26:41 -0300

    Update Boletim com listas compostas_2.py

--- Curso em Video/Mundo 03/Exercício 89/Boletim com listas compostas_2.py
+++ Curso em Video/Mundo 03/Exercício 89/Boletim com listas compostas_2.py
@@ -13,5 +13,12 @@ print(f'{"Nº":<4}{"Nome":<10}{"MÉDIA":>8}')
 print('-' * 30)
 for i, a in enumerate(ficha):
     print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
-
-
+while True:
+    print('-' * 30)
+    opc = int(input('Mostrar nota de qual aluno? (999 interrompe)'))
+    if opc == 999:
+        print('FINALIZANDO...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('VOLTE SEMPRE!!!')
