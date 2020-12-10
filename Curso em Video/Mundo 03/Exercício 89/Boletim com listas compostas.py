c1308e4a1294912dfcefa4e584c16d1e481da6c2
superpato20 <luizduck20@gmail.com>
2020-12-09 22:18:44 -0300

    Update Boletim com listas compostas.py

--- Curso em Video/Mundo 03/Exercício 89/Boletim com listas compostas.py
+++ Curso em Video/Mundo 03/Exercício 89/Boletim com listas compostas.py
@@ -2,8 +2,8 @@ listanome = list()
 listanome2 = list()
 while True:
     nome = str(input('Nome: '))
-    nota1 = int(input('Nota 1: '))
-    nota2 = int(input('Nota 2: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
     listanome.append(nome)
     listanome.append(nota1)
     listanome.append(nota2)
@@ -14,7 +14,9 @@ while True:
         continuar = str(input('Quer continuar? [S / N] ')).lower().strip()
     if continuar == 'n':
         break
+print('-=' * 15)
 print('Nº', 'NOME', 'MÉDIA')
-print('-' * 25)
+print('-' * 30)
 for index, item in enumerate(listanome2):
-    print(index, item)
+    print(index, item[0], (item[1] + item[2]) / 2)
+print('-' * 30)
