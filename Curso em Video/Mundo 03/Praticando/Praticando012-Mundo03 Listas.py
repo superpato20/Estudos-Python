4225fda728c3b9c143bea82448d7d29aec8c002b
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-12-11 10:54:42 -0300

    Update Praticando012-Mundo03 Listas.py

--- Curso em Video/Mundo 03/Praticando/Praticando012-Mundo03 Listas.py
+++ Curso em Video/Mundo 03/Praticando/Praticando012-Mundo03 Listas.py
@@ -4,12 +4,16 @@ print(f"O {dados['nome']} tem {dados['idade']} anos.")
 print(dados.keys())
 print(dados.values())
 print(dados.items())
+print('-' * 30)
 for k in dados:
     print(k)
+print('-' * 30)
 for k in dados.values():
     print(k)
+print('-' * 30)
 for k, v in dados.items():
     print(f'{k} = {v}')
+print('-' * 30)
 del dados['sexo']
 for k, v in dados.items():
     print(f'{k} = {v}')
