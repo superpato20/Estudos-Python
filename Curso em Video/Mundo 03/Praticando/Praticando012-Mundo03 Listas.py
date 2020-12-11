8dfb07210aca9b08ddbc414a5b2527082a5a4399
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-12-11 10:52:51 -0300

    Update Praticando012-Mundo03 Listas.py

--- Curso em Video/Mundo 03/Praticando/Praticando012-Mundo03 Listas.py
+++ Curso em Video/Mundo 03/Praticando/Praticando012-Mundo03 Listas.py
@@ -1,3 +1,15 @@
 dados = {'nome': 'Luiz', 'sexo': 'Masculino', 'idade': '36'}
 print(dados['nome'], dados['sexo'], dados['idade'])
 print(f"O {dados['nome']} tem {dados['idade']} anos.")
+print(dados.keys())
+print(dados.values())
+print(dados.items())
+for k in dados:
+    print(k)
+for k in dados.values():
+    print(k)
+for k, v in dados.items():
+    print(f'{k} = {v}')
+del dados['sexo']
+for k, v in dados.items():
+    print(f'{k} = {v}')
