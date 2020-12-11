5578879dcd8bd8b06fbfd3649bc14e6506915359
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-12-11 10:38:32 -0300

    Update Praticando012-Mundo03 Listas.py

--- Curso em Video/Mundo 03/Praticando/Praticando012-Mundo03 Listas.py
+++ Curso em Video/Mundo 03/Praticando/Praticando012-Mundo03 Listas.py
@@ -1,3 +1,3 @@
-dados = {'nome': 'Pedro', }
-print(dados['nome'])
-
+dados = {'nome': 'Luiz', 'sexo': 'Masculino', 'idade': '36'}
+print(dados['nome'], dados['sexo'], dados['idade'])
+print(f"O {dados['nome']} tem {dados['idade']} anos.")
