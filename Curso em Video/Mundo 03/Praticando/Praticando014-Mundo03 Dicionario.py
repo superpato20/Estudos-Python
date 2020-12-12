d3c431e219db61cba40fc7acd9f7c47d1957ff33
superpato20 <luizduck20@gmail.com>
2020-12-11 23:43:10 -0300

    Update Praticando014-Mundo03 Dicionario.py

--- Curso em Video/Mundo 03/Praticando/Praticando014-Mundo03 Dicionario.py
+++ Curso em Video/Mundo 03/Praticando/Praticando014-Mundo03 Dicionario.py
@@ -1,3 +1,19 @@
 estado = dict()
 brasil = list()
-
+for c in range(0, 3):
+    estado['urf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy())
+print(brasil)
+print('-' * 30)
+for c in brasil:
+    print(c)
+print('-' * 30)
+for e in brasil:
+    for k, v in e.items():
+        print(f'O campo {k} tem o valor {v}.')
+print('-' * 30)
+for e in brasil:
+    for v in e.values():
+        print(v, end=' ')
+    print()
