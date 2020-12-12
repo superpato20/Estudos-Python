3746f1f16e119e29141fa84e99788775c553ee3b
superpato20 <luizduck20@gmail.com>
2020-12-12 14:33:55 -0300

    Update Dicionário em Python.py

--- Curso em Video/Mundo 03/Exercício 90/Dicionário em Python.py
+++ Curso em Video/Mundo 03/Exercício 90/Dicionário em Python.py
@@ -1,2 +1,10 @@
-
-
+dados = dict()
+dados1 = list()
+dados['nome'] = str(input('Nome: '))
+dados['media'] = float(input(f"Média de {dados['nome']}: "))
+dados['situação'] = 'Aprovado' if dados['media'] >= 7 else 'Reprovado' if dados['media'] < 6 else 'Recuperação'
+dados1.append(dados.copy())
+for e in dados1:
+    for k, v in e.items():
+        print(f'- {k} é igual a '
+              f'{v} ')
