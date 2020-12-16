162399629c6f2ad526c4c82b89686de5da3094e4
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2020-12-16 17:52:55 -0300

    Update Jogo de Dados em Python.py

--- Curso em Video/Mundo 03/Exercício 91/Jogo de Dados em Python.py
+++ Curso em Video/Mundo 03/Exercício 91/Jogo de Dados em Python.py
@@ -1,12 +1,13 @@
 from random import randint
 from time import sleep
-
+jogadores2 = list()
 jogadores1 = dict()
 print('Valores sorteados:')
 for c in range(0, 4):
     jogadores1['sorteio'] = randint(1, 10)
     print(f"jogador{c} tirou {jogadores1['sorteio']} no dado.")
     sleep(0.9)
+
 
 print('-=' * 15)
 print(jogadores1)
