93b60a3c43531de08e62857571fb206793538655
superpato20 <luizduck20@gmail.com>
2020-12-25 10:01:34 -0300

    Update Jogo de Dados em Python.py

--- Curso em Video/Mundo 03/Exercício 91/Jogo de Dados em Python.py
+++ Curso em Video/Mundo 03/Exercício 91/Jogo de Dados em Python.py
@@ -1,14 +1,22 @@
 from random import randint
 from time import sleep
-jogadores2 = list()
-jogadores1 = dict()
+resultados = dict()
+jogadores = list()
 print('Valores sorteados:')
 for c in range(0, 4):
-    jogadores1['sorteio'] = randint(1, 10)
-    print(f"jogador{c} tirou {jogadores1['sorteio']} no dado.")
-    sleep(0.9)
-
-
-print('-=' * 15)
-print(jogadores1)
-
+    n = randint(1, 6)
+    resultados['Jogador' + str(c + 1)] = n
+    jogadores.append(n)
+    sleep(0.5)
+    print(f'O {"jogador" + str(c + 1)} tirou {n}')
+jogadores.sort(reverse=True)
+jogar = 't'
+print('Ranking dos Jogadores: ')
+for p in range(0, 4):
+    print(f'{p + 1}º Lugar: ', end='')
+    for k, v in resultados.items():
+        if v == jogadores[p] and jogar != k:
+            sleep(0.5)
+            print(k, 'com', v)
+            jogar = k
+            break
