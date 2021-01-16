1ddbb24768da800cb6ebaaeb432a4a939fd1e09d
Luiz Carlos <31809293+superpato20@users.noreply.github.com>
2021-01-16 09:13:26 -0300

    Update Cadastro de Trabalhador em Python.py

--- Curso em Video/Mundo 03/Exercício 92/Cadastro de Trabalhador em Python.py
+++ Curso em Video/Mundo 03/Exercício 92/Cadastro de Trabalhador em Python.py
@@ -1,2 +1,7 @@
-
+nome = str(input('Nome: '))
+nasc = int(input('Ano de Nascimento: '))
+cart = int(input('Carteira de Trabalho (0 não tem): '))
+if cart != 0:
+    anoc = str(input('Ano de Contratação: '))
+    salario = int(input('Salario: R$ '))
 
